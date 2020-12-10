80979eeae948d4bc6f7c8b6c54d77443a377bdc7
Danil <dkdottk@gmail.com>
2020-12-10 15:34:14 +0200

    Change from should to expect in Question specs

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  it { expect validate_presence_of :title }
+  it { expect validate_presence_of :body }
 end
